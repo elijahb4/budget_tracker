1f86121481a31d34e81ba0afdeffccca9d81a2ac
Elijah <elijah.j.bennett@gmail.com>
2025-07-04 16:09:13 +0100

    interest caluclation

---
+++ CalculateInterest.cs
@@ -0,0 +1,99 @@
+﻿using Mysqlx.Crud;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Individual_project_initial
+{
+    internal class CalculateInterest
+    {
+        private Dictionary<int, decimal> Earnings(int userpk)
+        {
+            var accountInterestDict = new Dictionary<int, decimal>();
+
+            try
+            {
+                using (var dbHelper = new DatabaseHelper())
+                {
+                    using (var connection = dbHelper.GetConnection())
+                    {
+                        string query = "SELECT accountpk, interestrate FROM accounts WHERE user_pk = @userpk";
+
+                        using (var command = new NpgsqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@userpk", userpk);
+                            using (var reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    int accountPK = reader.GetInt32(0);
+                                    decimal interestRate = reader.GetDecimal(1);
+                                    accountInterestDict[accountPK] = interestRate;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return new Dictionary<int, decimal>();
+            }
+
+            QueryTransactions(accountInterestDict.Keys.ToList());
+            return accountInterestDict;
+        }
+
+        private List<Transactionchange> QueryTransactions(List<int> accountPKs)
+        {
+            List<Transactionchange> allTransactions = new List<Transactionchange>();
+
+            foreach (int accountPK in accountPKs)
+            {
+                try
+                {
+                    using (var dbHelper = new DatabaseHelper())
+                    using (var connection = dbHelper.GetConnection())
+                    {
+                        string query = @"SELECT transactionpk, accountfk, transactionsum, transactiontime, 
+                                       balanceprior, balanceafter, logtype, newrate, rateprior
+                                       FROM transactions 
+                                       WHERE accountfk = @accountfk 
+                                       ORDER BY transactiontime ASC";
+
+                        using (var command = new NpgsqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@accountfk", accountPK);
+                            using (var reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    Transactionchange transaction = new Transactionchange
+                                    {
+                                        TransactionId = reader.GetInt32(0),
+                                        AccountFK = reader.GetInt32(1),
+                                        TransactionSum = reader.GetDecimal(2),
+                                        Timestamp = reader.GetDateTime(3),
+                                        BalanceBefore = reader.GetDecimal(4),
+                                        BalanceAfter = reader.GetDecimal(5),
+                                        LogType = reader.GetString(6)
+                                    };
+                                    allTransactions.Add(transaction);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            return allTransactions;
+        }
+    }
+}
--- Home.xaml.cs
+++ Home.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Npgsql;
+﻿using Mysqlx.Crud;
+using Npgsql;
 using System;
 using System.Data;
 using System.Diagnostics;
@@ -12,57 +13,16 @@ namespace Individual_project_initial
         public Home()
         {
             InitializeComponent();
-            UpdateInterest();
         }
-        private void UpdateInterest()
-        {
-            decimal totalInterestEarned = 0;
-            int owner = GetLoginOwner();
-            List<Int32> accounts = new List<Int32>();
-            List<decimal> balances = new List<decimal>();
-            try
-            {
-                using (var dbHelper = new DatabaseHelper())
-                {
-                    using (var connection = dbHelper.GetConnection())
-                    {
-                        string query = "SELECT accountpk, balance, accounttype FROM accounts WHERE owner = @Owner";
 
-                        using (var command = new NpgsqlCommand(query, connection))
-                        {
-                            command.Parameters.AddWithValue("@Owner", owner);
-                            using (var reader = command.ExecuteReader())
-                            {
-                                while (reader.Read())
-                                {
-                                    int accountPK = reader.GetInt32(0);
-                                    accounts.Add(accountPK);
-                                    decimal balance = reader.GetDecimal(1);
-                                    balances.Add(balance);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error retrieving account names when updating interest calculations: " + ex.Message);
-            }
-            decimal totalBalance = 0;
-            foreach (decimal balance in balances)
-            {
-                totalBalance += balance;
-            }
-            BalanceTextBlock.Text = "Total Balance: £" + totalBalance.ToString("F2");
-            foreach (Int32 AccountPK in accounts)
-            {
-                DateTime start = new DateTime(DateTime.Now.Year, 1, 1);
-                DateTime end = DateTime.Now;
-                InterestEarned(AccountPK, start, end);
-                totalInterestEarned += GetTotalInterest(AccountPK);
-            }
-            decimal tax_allowance = 0;
+        private void CalculateInterest()
+        {
+            int owner = GetLoginOwner();
+
+        }
+        
+        private void UpdateTotalInterestNotice()
+        {
             try
             {
                 using (var dbHelper = new DatabaseHelper())
@@ -115,199 +75,10 @@ namespace Individual_project_initial
             }
             InterestStackPanel.Children.Add(InterestStatus);
         }
+
         private int GetLoginOwner()
         {
             return Login.GetOwner();
-        }
-
-        public decimal InterestEarned(int AccountFK, DateTime startDate, DateTime endDate)
-        {
-            int owner = GetLoginOwner();
-            decimal totalInterestEarned = 0;
-            List<Transactionchange> transactions = new List<Transactionchange>();
-            decimal interestRate = 0;
-
-            try
-            {
-                using (var dbHelper = new DatabaseHelper())
-                {
-                    using (var connection = dbHelper.GetConnection())
-                    {
-                        string query = "SELECT interestrate FROM accounts WHERE accountpk = @AccountFK";
-
-                        using (var command = new NpgsqlCommand(query, connection))
-                        {
-                            command.Parameters.AddWithValue("@AccountFK", AccountFK);
-                            using (var reader = command.ExecuteReader())
-                            {
-                                if (reader.Read())
-                                {
-                                    interestRate = reader.GetDecimal(0);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error retrieving interest rate: " + ex.Message);
-            }
-
-            try
-            {
-                using (var dbHelper = new DatabaseHelper())
-                {
-                    using (var connection = dbHelper.GetConnection())
-                    {
-                        string query = "SELECT * FROM transactions WHERE AccountFK = @accountFK ORDER BY TransactionTime ASC";
-
-                        using (var command = new NpgsqlCommand(query, connection))
-                        {
-                            command.Parameters.AddWithValue("@accountfk", AccountFK);
-                            using (var reader = command.ExecuteReader())
-                            {
-                                while (reader.Read())
-                                {
-                                    Transactionchange transaction = new Transactionchange
-                                    {
-                                        AccountFK = reader.GetInt32(1),
-                                        TransactionSum = reader.GetDecimal(2),
-                                        Timestamp = reader.GetDateTime(3),
-                                        BalanceAfter = reader.GetDecimal(4),
-                                        BalanceBefore = reader.GetDecimal(5),
-                                        LogType = reader.GetString(6),
-                                    };
-                                    transactions.Add(transaction);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error retrieving transactions: " + ex.Message);
-            }
-
-            DateTime workingDate = new DateTime();
-            int txIndex = 0;
-            decimal balance = 0;
-            decimal dailyInterest = 0;
-            decimal balanceprior = 0;
-
-            balance = GetBalanceBeforeDate(AccountFK, startDate);
-
-            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
-            {
-                while (txIndex < transactions.Count && transactions[txIndex].Timestamp.Date == date.Date)
-                {
-                    balance += transactions[txIndex].TransactionSum;
-                    txIndex++;
-                }
-                dailyInterest = balance * (interestRate / 100 / 365);
-                totalInterestEarned += dailyInterest;
-                decimal balanceafter = balance + dailyInterest;
-                InsertInterest(AccountFK, dailyInterest, date, balanceafter, balance);
-                balance = balanceafter;
-            }
-
-            return totalInterestEarned;
-        }
-        private void InsertInterest(int AccountFK, decimal transactionSum, DateTime timestamp, decimal balanceafter, decimal balanceprior)
-        {
-            string logType = "Interest";
-            try
-            {
-                using (var dbHelper = new DatabaseHelper())
-                {
-                    using (var connection = dbHelper.GetConnection())
-                    {
-                        string query = "INSERT INTO transactions (accountfk, transactionsum, transactiontime, balanceafter, balanceprior, logtype) " +
-                            "VALUES (@AccountFK, @TransactionSum, @TransactionTime, @balanceafter, @balanceprior, @logtype)";
-                        using (var command = new NpgsqlCommand(query, connection))
-                        {
-                            command.Parameters.AddWithValue("@AccountFK", AccountFK);
-                            command.Parameters.AddWithValue("@TransactionSum", transactionSum);
-                            command.Parameters.AddWithValue("@TransactionTime", timestamp);
-                            command.Parameters.AddWithValue("@balanceafter", balanceafter);
-                            command.Parameters.AddWithValue("@balanceprior", balanceprior);
-                            command.Parameters.AddWithValue("@logtype", logType);
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error inserting interest: " + ex.Message);
-            }
-        }
-
-        public static decimal GetTotalInterest(int accountId)
-        {
-            using (var dbHelper = new DatabaseHelper())
-            {
-                using (var connection = dbHelper.GetConnection())
-                {
-                    using var cmd = new NpgsqlCommand(@"SELECT COALESCE(SUM(transactionsum), 0) FROM public.transactions WHERE accountfk = @AccountFK AND logtype = @logtype;", connection);
-                    {
-                        cmd.Parameters.AddWithValue("@accountfk", accountId);
-                        cmd.Parameters.AddWithValue("@logtype", "Interest");
-
-                        var result = cmd.ExecuteScalar();
-                        return result != DBNull.Value ? Convert.ToDecimal(result) : 0m;
-                    }
-                }
-            }
-        }
-        private decimal GetBalanceBeforeDate(int AccountFK, DateTime startDate)
-        {
-            try
-            {
-                using (var dbHelper = new DatabaseHelper())
-                using (var connection = dbHelper.GetConnection())
-                {
-                    string query = @"SELECT balanceafter 
-                        FROM transactions 
-                        WHERE accountfk = @AccountFK 
-                        AND transactiontime < @StartDate 
-                        ORDER BY transactiontime DESC LIMIT 1";
-
-                    using (var command = new NpgsqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@AccountFK", AccountFK);
-                        command.Parameters.AddWithValue("@StartDate", startDate);
-
-                        using (var reader = command.ExecuteReader())
-                        {
-                            if (reader.Read())
-                            {
-                                return reader.GetDecimal(0);
-                            }
-                        }
-                    }
-
-                    query = "SELECT balance FROM accounts WHERE accountpk = @AccountFK";
-                    using (var command = new NpgsqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@AccountFK", AccountFK);
-                        using (var reader = command.ExecuteReader())
-                        {
-                            if (reader.Read())
-                            {
-                                return reader.GetDecimal(0);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error retrieving balance: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            return 0;
         }
 
         private void GoToAccounts_Click(object sender, RoutedEventArgs e)
